f7bd18b7094932b16bbdc6b985f9e60d9f32a98a
Mark <60570844+Marcus228@users.noreply.github.com>
2026-05-28 12:51:30 +0100

    feat: added a general config file.

--- constants.py
+++
@@ -1,4 +0,0 @@
-from typing import Literal
-
-AllowedAirlines = Literal["Virgin Atlantic", "Swiss", "Lufthansa", "Turkish", "KLM", "Air France",
-                          "British Airways", "Air India", "Emirates", "Etihad"]
