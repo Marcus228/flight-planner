c355f249099c465515e4c7425f5400177b9f7aaf
Mark <60570844+Marcus228@users.noreply.github.com>
2026-05-28 21:25:44 +0100

    format: added clarifying comments to the constants and functions

--- core/config.py
+++ core/config.py
@@ -3,8 +3,11 @@ from typing import Literal
 AllowedAirlines = Literal["Virgin Atlantic", "Swiss", "Lufthansa", "Turkish", "KLM", "Air France",
                           "British Airways", "Air India", "Emirates", "Etihad"]
 
-# max retry threshold configuration
+# max retry threshold configuration for extractor node
+#   if the model fails to provide the reply after MAX_EXTRACT_REPLIES,
+#   the timeout is declared
 MAX_EXTRACT_RETRIES = 3
 
-# SerpApi Endpoint for Google Flights
-SERPAPI_URL = "https://serpapi.com/search"
+# BATCHING LOGIC for SerpAPI in fetcher node.
+# The API allows concurrency, but batching is defaulted to size 5 to be safe
+BATCH_SIZE = 5
--- core/llm.py
+++ core/llm.py
@@ -1,6 +1,8 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 
 # returns a structured LLM compatible with LangChain technology
+# choice of llm can be done by altering the imports and llm variable
+# NOTE: the respective API key has to be present in .env file: LLM_API_KEY=YOUR_API_KEY_GOES_HERE
 def get_structured_llm(schema, temperature: float = 0.0):
     """
     Centralized factory for instantiating the LLM and binding it to a schema.
