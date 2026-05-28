34915f78a1bff13222b6eeb429a8c387d86a696a
Mark <60570844+Marcus228@users.noreply.github.com>
2026-05-28 22:09:17 +0100

    fix: API key not found. Solved by explicitly injecting API key.

--- core/llm.py
+++ core/llm.py
@@ -1,4 +1,9 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
+import os
+from dotenv import load_dotenv
+
+# fetch API keys
+load_dotenv()
 
 # returns a structured LLM compatible with LangChain technology
 # choice of llm can be done by altering the imports and llm variable
@@ -8,7 +13,17 @@ def get_structured_llm(schema, temperature: float = 0.0):
     Centralized factory for instantiating the LLM and binding it to a schema.
     Easily hot-swappable for different models or providers.
     """
-    # If you change models in the future, you only change this line
-    llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash", temperature=temperature)
+    # Fetch the LLM API key from .env
+    custom_key = os.environ.get("LLM_API_KEY")
+
+    if not custom_key:
+        raise ValueError("CRITICAL: LLM_API_KEY environment variable is not set.")
+
+    # Inject the key explicitly using the api_key parameter
+    llm = ChatGoogleGenerativeAI(
+        model="gemini-2.5-flash",
+        temperature=temperature,
+        api_key=custom_key
+    )
 
     return llm.with_structured_output(schema, method="json_schema")
