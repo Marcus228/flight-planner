41d0984eaedae1a539a8232a014f2db9f3e20b6f
Mark <60570844+Marcus228@users.noreply.github.com>
2026-05-31 17:33:17 +0100

    format: removed dead constants

--- core/config.py
+++ core/config.py
@@ -1,11 +1,11 @@
-from typing import List
+from typing import Set
 from enum import Enum
 
 type AirlineShortcode = str
 
 # allowed airlines to infer the information about
 # the shortcodes were taken from https://www.iata.org/en/publications/directories/code-search?
-allowedAirlines: List[AirlineShortcode] = [
+allowedAirlines: Set[AirlineShortcode] = {
     # Virgin Atlantic Airways Ltd
     "VS",
     # SWISS International Air Lines Ltd.
@@ -26,7 +26,7 @@ allowedAirlines: List[AirlineShortcode] = [
     "EK",
     # Etihad Airways Dba Etihad Airways PJSC
     "EY"
-]
+}
 
 # travel classification enum class
 # used to extract flights of the specific class
@@ -41,10 +41,6 @@ class TravelClass(str, Enum):
 #   the timeout is declared
 MAX_EXTRACT_RETRIES = 3
 
-# BATCHING LOGIC for SerpAPI in fetcher node.
+# used for prevention of SerpAPI gateway closure in mcp_fetcher
 # The API allows concurrency, but batching is defaulted to size 5 to be safe
-BATCH_SIZE = 5
-
-# determines how many top results to retrieve per API call
-# 2 is recommended to ensure state is manageable
-TOP_RESULTS = 2
+MAX_CONCURRENT_API_CALLS = 10
